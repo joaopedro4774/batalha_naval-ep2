79c6d6c5f3d9ae4b9e7e238e5758c895e5141e0d
joaopedro4774 <116032546+joaopedro4774@users.noreply.github.com>
2023-04-25 16:17:39 -0300

    posiciona_frota

---
+++ posiciona_frota.py
@@ -0,0 +1,20 @@
+def posiciona_frota(frota):
+
+    grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    for key in frota:
+        for coordenadas_lista in frota[key]:
+            for coordenadas in coordenadas_lista:
+                i, j = coordenadas
+                grid[i][j] = 1
+
+    return grid
